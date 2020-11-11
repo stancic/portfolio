19bbf9a0f973a0152096f63d7e0804fe7f6ffa54
stancic <dinostancic7@gmail.com>
2020-11-11 18:46:15 +0100

    Created simple api for editing projects

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,12 @@ const app = express()
 const cors = require('cors')
 const logger = require('./utils/logger')
 const mongoose  = require('mongoose')
+const middleware = require('./utils/middleware')
+
+//Routers
+const projectsRouter = require('./controllers/projects_controller')
+
+
 
 mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 		.then(() => logger.info('connected to MongoDB'))
@@ -14,6 +20,11 @@ mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology:
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
+app.use('/api/projects', projectsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- backend/controllers/projects_controller.js
+++ backend/controllers/projects_controller.js
@@ -1,8 +1,35 @@
 const projectsRouter = require('express').Router()
 const Project = require('../models/projects_model')
 
+
 //Get all projects
 projectsRouter.get('/', async(request, response) => {
 	const projects = await Project.find({})
 	response.json(projects)
 })
+
+//Add projects
+projectsRouter.post('/addproject', async(request, response) => {
+	const body = request.body
+
+	const project = new Project({
+		title: body.title,
+		git: body.git,
+		live_preview: body.live_preview,
+		description: body.description
+	})
+
+	const savedProject = await project.save()
+	response.json(savedProject)
+})
+
+
+//Remove projects
+projectsRouter.delete('/deleteproject/:id', async (request, response) => {
+	const id = request.params.id
+	const project = await Project.findByIdAndRemove(id)
+	console.log('deleted', request.params)
+	response.json(project)
+})
+
+module.exports = projectsRouter
