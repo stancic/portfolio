7dd19abd75d1de05a361229cde3e41c730b718a8
stancic <dinostancic7@gmail.com>
2020-11-11 18:15:23 +0100

    Add mongoose connect to database

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,12 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const logger = require('./utils/logger')
+const mongoose  = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+		.then(() => logger.info('connected to MongoDB'))
+		.catch(error => logger.error('error connecting to MongoDB: ', error.message))
 
 
 app.use(cors())
