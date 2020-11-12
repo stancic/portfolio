ada8fd2bef05ddd0e9d951418d363f2408e88407
stancic <dinostancic7@gmail.com>
2020-11-12 15:21:32 +0100

    Add form for project adding

--- database-frontend/src/components/Projects.js
+++ database-frontend/src/components/Projects.js
@@ -1,19 +1,53 @@
-import React from 'react'
-import { Table, Button } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Table, Button, Form } from 'react-bootstrap'
 import { useDispatch } from 'react-redux'
-import { deleteProject } from '../reducers/projectsReducer'
+import { deleteProject, addProject } from '../reducers/projectsReducer'
+
+/********** PROJECT MODEL*************
+title: body.title,
+git: body.git,
+live_preview: body.live_preview,
+img_link: body.img_link,
+description: body.description
+*********** PROJECT MODEL*************/
+
 
 
 function Projects({ projects }) {
+
+	const [title, setTitle] = useState('')
+	const [gitLink, setGitLink] = useState('')
+	const [livePreviewLink, setLivePreviewLink] = useState('')
+	const [imgLink, setImgLink] = useState('')
+	const [description, setDescription] = useState('')
 
 	const dispatch = useDispatch()
 
 	const handleDelete = (project) => {
 		dispatch(deleteProject(project))
 	}
-	
+
+
+	const handleAdding = (event) => {
+		event.preventDefault()
+		const projectToAdd = {
+			title: title,
+			git: gitLink,
+			live_preview: livePreviewLink,
+			img_link: imgLink,
+			description: description
+		}
+		console.log(projectToAdd)
+		dispatch(addProject(projectToAdd))
+		setTitle('')
+		setGitLink('')
+		setLivePreviewLink('')
+		setImgLink('')
+		setDescription('')
+	}
 	return (
-		<Table striped bordered variant="dark">
+		<div>
+			<Table striped bordered variant="dark">
 				<thead>
 					<tr>
 						<th>Title</th>
@@ -29,13 +63,28 @@ function Projects({ projects }) {
 						<td>{project.title}</td>
 						<td>{project.git}</td>
 						<td>{project.live_preview}</td>
-						<td><a href={project.img_path} target="blank">{project.img_path}</a></td>
+						<td><a href={project.img_link} target="blank">{project.img_link}</a></td>
 						<td><Button variant="danger" onClick={() => handleDelete(project)}>Remove</Button></td>
 					</tr>
 				)
 				}
 				</tbody>
 			</Table>
+			<Form onSubmit={handleAdding}>
+				<br />
+				<Form.Control type="text" placeholder="Title" onChange={({target}) => setTitle(target.value)}/>
+				<br />
+				<Form.Control type="text" placeholder="GitHub/Lab link" onChange={({target}) => setGitLink(target.value)}/>
+				<br />
+				<Form.Control type="text" placeholder="Live preview link" onChange={({target}) => setLivePreviewLink(target.value)}/>
+				<br />
+				<Form.Control type="text" placeholder="Image link" onChange={({target}) => setImgLink(target.value)}/>
+				<br />
+				<Form.Control type="text" placeholder="Description" onChange={({target}) => setDescription(target.value)}/>
+				<br />
+				<Button type="submit" variant="primary">Add to database</Button>
+			</Form>
+		</div>
 	)
 }
 
