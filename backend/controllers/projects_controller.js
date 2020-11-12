0243468d5d51551053b3f23695e93e9a1cadf259
stancic <dinostancic7@gmail.com>
2020-11-12 15:18:21 +0100

    Change "img_path" to "img_link"

--- backend/controllers/projects_controller.js
+++ backend/controllers/projects_controller.js
@@ -16,7 +16,7 @@ projectsRouter.post('/addproject', async(request, response) => {
 		title: body.title,
 		git: body.git,
 		live_preview: body.live_preview,
-		img_link: body.img_path,
+		img_link: body.img_link,
 		description: body.description
 	})
 
