c52e871e172539dfb282dbf167654d1f43fdb3d9
stancic <dinostancic7@gmail.com>
2020-11-12 16:08:37 +0100

    Add user model

---
+++ backend/models/user_model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+	username:{
+		type: String,
+		required: true,
+		minlength: 4,
+		unique: true
+	},
+	passwordHash:{
+		type: String,
+		minlength: 3,
+		required: true
+	}
+})
+
+
+userSchema.plugin(uniqueValidator)
+userSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+		delete returnedObject.passwordHash
+	}
+})
+
+
+const User = mongoose.model('User',userSchema)
+module.exports = User
