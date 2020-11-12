87ac622bddd2a3d2e42184d831c4caee89bc0104
stancic <dinostancic7@gmail.com>
2020-11-12 11:15:37 +0100

    Using redux store for getting projects

--- database-frontend/src/App.js
+++ database-frontend/src/App.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { initializeProjects } from './reducers/projectsReducer'
+import { useSelector, useDispatch } from 'react-redux'
+
 
 function App() {
+	const dispatch = useDispatch()
+
+	//Redux for getting projects
+	useEffect(()=>{
+		dispatch(initializeProjects())
+	},[dispatch])
+	
+	const projects = useSelector(state => state.projects)
+	console.log(projects)
 
 	return (
 		<div>
