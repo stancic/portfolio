ae19d25659b5c752c68a181fb894baf19ceec0b1
stancic <dinostancic7@gmail.com>
2020-11-14 11:31:44 +0100

    Added local storage for saving user login

--- database-frontend/src/store.js
+++ database-frontend/src/store.js
@@ -3,14 +3,52 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import projectsReducer from './reducers/projectsReducer'
 import loginReducer from './reducers/loginReducer'
+import throttle from 'lodash/throttle'
+
+//local storage for saving user
+
+const loadState = () => {
+	try{
+		const serializedState = localStorage.getItem('user')
+		if(serializedState === null){
+			return undefined
+		}
+		else return JSON.parse(serializedState)
+	} catch(error){
+		console.log(error)
+		return undefined
+	}
+}
+
+
+//saving to local storage
+
+const saveState = (state) => {
+	try{
+		const serializedState = JSON.stringify(state)
+		localStorage.setItem('user', serializedState)
+	} catch(error){
+		//just ignore if error happens
+		console.log(error)
+	}
+}
+
+const persistedState = loadState()
+
 
 const reducer = combineReducers({
 	projects: projectsReducer,
 	user: loginReducer
 })
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(reducer, persistedState, composeWithDevTools(applyMiddleware(thunk)))
 
+//avoids excessively store subscribing with throttle
+store.subscribe(throttle(() => {
+	saveState({
+		user: store.getState().user
+	})
+}, 1000))
 
 //Just for the purpose of testing what store's output is
 console.log('store',store.getState())
