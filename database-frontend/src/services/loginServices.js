0f09a94ac091f1ecdfe6eb1b3a8820e4c5fe0cab
stancic <dinostancic7@gmail.com>
2020-11-14 13:34:01 +0100

    Removed console.log()

--- database-frontend/src/services/loginServices.js
+++ database-frontend/src/services/loginServices.js
@@ -3,7 +3,6 @@ const baseURL = 'http://localhost:3333/customcms/api/login/ec98f81637370f86db84c
 
 const login = async credentials => {
 	const response = await axios.post(baseURL, credentials)
-	console.log('login service: ', response.data)
 	return response.data
 }
 
