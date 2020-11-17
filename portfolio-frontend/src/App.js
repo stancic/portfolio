ec9cc63abfea21240cd3615aeddfaa6ebdcf8f0b
stancic <dinostancic7@gmail.com>
2020-11-17 18:35:11 +0100

    Add landing page and edit background color

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -7,16 +7,18 @@ import './App.scss'
 //IMPORT COMPONENTS
 import Navigation from './components/navigation-bar/Navigation'
 import Socials from './components/socials/Socials'
+import LandingPage from './components/landing_page/LandingPage'
 
 //DATA OBJECTS IMPORTING
-import {linksEN, linksHR} from './components/data_objects/links'
-
+import { linksEN, linksHR } from './components/data_objects/links'
+import { dataEN } from './components/data_objects/landingPageData'
 
 function App() {
 	return (
 		<div>
 			<Navigation {...linksEN}/>
 			<Socials />
+			<LandingPage {...dataEN}/>
 		</div>
 	)
 }
