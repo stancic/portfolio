6735bf9800b1e95e327ac1f5a35d995d25b3ccd9
stancic <dinostancic7@gmail.com>
2020-11-17 18:34:12 +0100

    Add landing page details

---
+++ portfolio-frontend/src/components/data_objects/landingPageData.js
@@ -0,0 +1,6 @@
+export const dataEN = {
+	"title": ["Hello!", "Welcome!"],
+	"description": "I'm Dino, a student and web developer in development.",
+	"contact": "Contact me!",
+	"download": "Download CV!"
+}
