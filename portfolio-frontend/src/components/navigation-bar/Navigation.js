82490641b6cee2fe0d86484e5a2c1a06e1787df6
stancic <dinostancic7@gmail.com>
2020-11-19 18:20:30 +0100

    Add dispatch for load state

--- portfolio-frontend/src/components/navigation-bar/Navigation.js
+++ portfolio-frontend/src/components/navigation-bar/Navigation.js
@@ -1,12 +1,15 @@
 //IMPORT DEPENDENCIES
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
 //IMPORT STYLES
 import './navigation.scss'
+import { loadPage } from '../../reducers/loadingPageReducer'
 
 
 function Navigation({title, first_link, second_link}) {
+	const dispatch = useDispatch()
 	const addStyle = () => {
 		const cursor = document.querySelector('.custom-cursor')
 		cursor.classList.add("grow-cursor")
@@ -15,7 +18,6 @@ function Navigation({title, first_link, second_link}) {
 		const cursor = document.querySelector('.custom-cursor')
 		cursor.classList.remove("grow-cursor")
 	}
-	console.log(first_link.link)
 
 	return (
 		<div>
@@ -26,12 +28,17 @@ function Navigation({title, first_link, second_link}) {
 
 				<div className="navigation-container" >
 					<ul className="links">
-						<li className="list-link-item" onMouseEnter={addStyle} onMouseLeave={removeStyle}>
-							<Link to={first_link.link}>{first_link.data}</Link>
-						</li>
-						<li className="list-link-item" onMouseEnter={addStyle} onMouseLeave={removeStyle}>
-							<Link to={second_link.link}>{second_link.data}</Link>
-						</li>
+							<Link to={first_link.link} onClick={() => dispatch(loadPage())} className="link">
+								<li className="list-link-item" onMouseEnter={addStyle} onMouseLeave={removeStyle}>
+									{first_link.data}
+								</li>
+							</Link>
+
+							<Link to={second_link.link} onClick={() => dispatch(loadPage())} className="link">
+								<li className="list-link-item" onMouseEnter={addStyle} onMouseLeave={removeStyle}>
+									{second_link.data}
+								</li>
+							</Link>
 					</ul>
 				</div>
 				<div className="icon-container">
