ffb18df6224ad5ce7793793aea5861613ac8a490
stan <dinostancic7@gmail.com>
2020-12-08 16:22:01 +0100

    Add working notification

--- database-frontend/src/components/Notification.js
+++ database-frontend/src/components/Notification.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { logout } from '../reducers/loginReducer'
 import { hideNotification } from '../reducers/notificationStatusReducer'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 
+import './Notification.scss'
 
 function Notification({message}) {
 	const dispatch = useDispatch()
@@ -11,11 +13,22 @@ function Notification({message}) {
 		dispatch(hideNotification())
 		dispatch(logout())
 	}
+
+	useEffect(()=>{
+		setTimeout(()=>{
+			dispatch(hideNotification())
+			dispatch(logout())
+		}, 5000)
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	},[notificationState])
+
 	return (
-		<div style={notificationState ? {display: 'block'} : {display: 'none'}}>
-			<div>
-				<h1>DALJE NECES MOCI</h1>
-				<button onClick={handleClick}>Sakrij</button>
+		<div className="notification-container" style={notificationState ? {top: '15%'} : {top: '-15%'}}>
+			<div className="notification-message-container">
+				<p className="notification-message">wrong credentials...</p>
+			</div>
+			<div className="close-notification-container">
+				<AiOutlineCloseCircle onClick={handleClick}/>
 			</div>
 		</div>
 	)
