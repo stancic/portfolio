209e5bc135ceceb964af5e2943718757e86eaac8
stan <dinostancic7@gmail.com>
2020-12-08 18:33:53 +0100

    Add more welcome messages

--- portfolio-frontend/src/components/data_objects/landingPageData.js
+++ portfolio-frontend/src/components/data_objects/landingPageData.js
@@ -1,5 +1,5 @@
 export const dataEN = {
-	"title": ["Hello!", "Welcome!"],
+	"title": ["Hello!", "Pozdrav!", "Welcome!", "Ciao!", "Oi!"],
 	"description": "I'm Dino, a student and web developer in development.",
 	"contact": "Contact me!",
 	"download": "Download CV!"
