10bd97a0fd1e8e077ab724bd1a9a6f24a0f53647
Stan <dinostancic7@gmail.com>
2021-01-18 11:55:10 +0100

    Changed CV

--- portfolio-frontend/src/components/landing_page/LandingPage.js
+++ portfolio-frontend/src/components/landing_page/LandingPage.js
@@ -97,7 +97,7 @@ function LandingPage({title, description, contact, download}){
 						<Link to="/contact_me">
 							<button className="contact-button">{contact}</button>
 						</Link>
-						<a href="https://drive.google.com/file/d/1PJA7rZpjLaAgJiENVXglDka1vQpi2RQq/view?usp=sharing" target="_blank" rel="noreferrer">
+						<a href="https://drive.google.com/file/d/1IHfaSR8ZH4xj_MRI-W3iGXyBZklJgIAA/view" target="_blank" rel="noreferrer">
 							<button className="download-button">Download CV!</button>
 						</a>
 					</div>
